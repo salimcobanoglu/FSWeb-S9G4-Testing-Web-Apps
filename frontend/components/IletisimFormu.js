167a4403b38d2ce3a9025023fba00f632b6a5f57
Salim Çobanoğlu <salimcobanoglu@yahoo.com>
2023-04-25 13:42:02 +0200

    done

--- frontend/components/IletisimFormu.js
+++ frontend/components/IletisimFormu.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import Goruntule from './Goruntule';
+import Goruntule from "./Goruntule";
 
 const formData = {
   ad: "",
   soyad: "",
   email: "",
-  mesaj: ""
+  mesaj: "",
 };
 
 const errorData = {
   ad: "",
   soyad: "",
   email: "",
-  mesaj: ""
+  mesaj: "",
 };
 
 const IletisimFormu = () => {
@@ -21,8 +21,8 @@ const IletisimFormu = () => {
   const [errors, setErrors] = useState(errorData);
 
   const errorHandling = (fieldName, fieldValue) => {
-    if (fieldName === "ad" && fieldValue.length < 5)
-      return `${fieldName} en az 5 karakter olmalıdır.`;
+    if (fieldName === "ad" && fieldValue.length < 3)
+      return `${fieldName} en az 3 karakter olmalıdır.`;
 
     const emailRegex = /(.*)@(.*)\.(.+)/g;
     if (fieldName === "email" && !fieldValue.match(emailRegex))
@@ -34,20 +34,22 @@ const IletisimFormu = () => {
     return "";
   };
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const submitErrors = {};
-    Object.keys(errors).forEach(field => {
+    Object.keys(errors).forEach((field) => {
       submitErrors[field] = errorHandling(field, form[field]);
     });
 
     setErrors(submitErrors);
 
-    const hasErrors = (submitErrors.ad === "" && submitErrors.soyad === "" && submitErrors.email === "" && submitErrors.mesaj === "");
+    const hasErrors =
+      submitErrors.ad === "" &&
+      submitErrors.soyad === "" &&
+      submitErrors.email === "" &&
+      submitErrors.mesaj === "";
     setDisplayData(hasErrors);
-
   };
 
   const handleChange = (e) => {
@@ -59,12 +61,12 @@ const IletisimFormu = () => {
 
     setErrors({
       ...errors,
-      [e.target.name]: errorMessage
+      [e.target.name]: errorMessage,
     });
 
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -79,9 +81,9 @@ const IletisimFormu = () => {
             name="ad"
             value={form.ad}
             id="ad"
-            placeholder="İlhan"
+            placeholder="salim"
           />
-          {(errors.ad) && <p data-testid="error">Hata: {errors.ad}</p>}
+          {errors.ad && <p data-testid="error-name">Hata: {errors.ad}</p>}
         </div>
 
         <div>
@@ -91,9 +93,11 @@ const IletisimFormu = () => {
             id="soyad"
             name="soyad"
             value={form.soyad}
-            placeholder="Mansız"
+            placeholder="cobanoglu"
           />
-          {(errors.soyad) && <p data-testid="error">Hata: {errors.soyad}</p>}
+          {errors.soyad && (
+            <p data-testid="error-surname">Hata: {errors.soyad}</p>
+          )}
         </div>
 
         <div>
@@ -103,9 +107,11 @@ const IletisimFormu = () => {
             id="email"
             name="email"
             value={form.email}
-            placeholder="yüzyılıngolcüsü@hotmail.com"
+            placeholder="workintech@hotmail.com"
           />
-          {(errors.email) && <p data-testid="error">Hata: {errors.email}</p>}
+          {errors.email && (
+            <p data-testid="error-email">Hata: {errors.email}</p>
+          )}
         </div>
 
         <div>
@@ -116,12 +122,14 @@ const IletisimFormu = () => {
             id="mesaj"
             value={form.mesaj}
           />
-          {(errors.mesaj) && <p data-testid="error">Error: {errors.mesaj}</p>}
+          {errors.mesaj && (
+            <p data-testid="error-message">Error: {errors.mesaj}</p>
+          )}
         </div>
 
-        {displayData && <Goruntule form={form}/>}
+        {displayData && <Goruntule form={form} />}
 
-        <button>Gönder</button>
+        <button data-testid="submit-button">Gönder</button>
       </form>
     </div>
   );
--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -1,41 +1,123 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
-import IletisimFormu from './IletisimFormu';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import IletisimFormu from "./IletisimFormu";
 
-test('hata olmadan render ediliyor', () => {
-
+test("hata olmadan render ediliyor", () => {
+  render(<IletisimFormu />);
 });
 
-test('iletişim formu headerı render ediliyor', () => {
+test("iletişim formu headerı render ediliyor", () => {
+  render(<IletisimFormu />);
 
+  const header = screen.getByText(/İletişim Formu/i);
+  expect(header).toBeInTheDocument();
 });
 
-test('kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.', async () => {
-
+test("kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.", async () => {
+  render(<IletisimFormu />);
+  const kullaniciAdi = screen.getByPlaceholderText(/İlhan/i);
+  userEvent.type(kullaniciAdi, "Jo");
+  const errorMessage = screen.getByTestId("error-name");
+  expect(errorMessage).toBeInTheDocument();
 });
 
-test('kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.', async () => {
+test("kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const submitButton = screen.getByTestId("submit-button");
+  userEvent.click(submitButton);
 
+  const nameErrorMessage = screen.getByTestId("error-name");
+  expect(nameErrorMessage).toBeInTheDocument();
+
+  const surnameErrorMessage = screen.getByTestId("error-surname");
+  expect(surnameErrorMessage).toBeInTheDocument();
+
+  const emailErrorMessage = screen.getByTestId("error-email");
+  expect(emailErrorMessage).toBeInTheDocument();
 });
 
-test('kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.', async () => {
+test("kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const kullaniciAdi = screen.getAllByPlaceholderText(/İlhan/i);
+  userEvent.type(kullaniciAdi, "Burcu");
 
+  const kullaniciSoyadi = screen.getAllByPlaceholderText(/Mansız/i);
+  userEvent.type(kullaniciSoyadi, "Deniz");
+
+  const submitButton = screen.getByTestId("submit-button");
+  userEvent.click(submitButton);
+
+  const emailErrorMessage = screen.getByTestId("error-email");
+  expect(emailErrorMessage).toBeInTheDocument();
 });
 
 test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
-
+  render(<IletisimFormu />);
+  const kullaniciEmail = screen.getByPlaceholderText(
+    /yüzyılıngolcüsü@hotmail.com/i
+  );
+  userEvent.type(kullaniciEmail, "mail");
+  const emailErrorMessage = screen.getByTestId("error-email");
+  expect(emailErrorMessage).toHaveTextContent(
+    "email geçerli bir email adresi olmalıdır."
+  );
 });
 
 test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
+  render(<IletisimFormu />);
+  const kullaniciSoyadi = screen.getByPlaceholderText(/Mansız/i);
+  userEvent.type(kullaniciSoyadi, "d");
+  userEvent.clear(kullaniciSoyadi);
 
+  const kullaniciSoyadiMessage = screen.getByTestId("error-surname");
+  expect(kullaniciSoyadiMessage).toHaveTextContent("soyad gereklidir.");
 });
 
-test('ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.', async () => {
+test("ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.", async () => {
+  render(<IletisimFormu />);
 
+  const kullaniciAdi = screen.getByPlaceholderText(/İlhan/i);
+  userEvent.type(kullaniciAdi, "burcu");
+
+  const kullaniciSoyadi = screen.getByPlaceholderText(/Mansız/i);
+  userEvent.type(kullaniciSoyadi, "deniz");
+
+  const kullaniciEmail = screen.getByPlaceholderText(
+    /yüzyılıngolcüsü@hotmail.com/i
+  );
+  userEvent.type(kullaniciEmail, "dnzzburcu@gmail.com");
+  const submitButton = screen.getByTestId("submit-button");
+  userEvent.click(submitButton);
+  await waitFor(() => {
+    const errorDiv = screen.queryAllByTestId("error");
+    expect(errorDiv.length).toBe(0);
+  });
 });
 
-test('form gönderildiğinde girilen tüm değerler render ediliyor.', async () => {
+test("form gönderildiğinde girilen tüm değerler render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const kullaniciAdi = screen.getByPlaceholderText(/İlhan/i);
+  userEvent.type(kullaniciAdi, "burcu");
 
+  const kullaniciSoyadi = screen.getByPlaceholderText(/Mansız/i);
+  userEvent.type(kullaniciSoyadi, "deniz");
+
+  const kullaniciEmail = screen.getByPlaceholderText(
+    /yüzyılıngolcüsü@hotmail.com/i
+  );
+  userEvent.type(kullaniciEmail, "dnzzburcu@gmail.com");
+  const submitButton = screen.getByTestId("submit-button");
+  userEvent.click(submitButton);
+  await waitFor(() => {
+    const displayName = screen.getByTestId("firstnameDisplay");
+    expect(displayName).toHaveTextContent("burcu");
+
+    const displaySurname = screen.getByTestId("lastnameDisplay");
+    expect(displaySurname).toHaveTextContent("deniz");
+
+    const displayEmail = screen.getByTestId("emailDisplay");
+    expect(displayEmail).toHaveTextContent("dnzzburcu@gmail.com");
+  });
 });
